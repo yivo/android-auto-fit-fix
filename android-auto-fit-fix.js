c9f9461c9ab5a50b19aac21aaebc1777a41cc8a9
Yaroslav Konoplov <eahome00@gmail.com>
2017-01-11 21:27:04 +0200

    Reformat code

--- android-auto-fit-fix.js
+++ android-auto-fit-fix.js
@@ -1,17 +1,32 @@
 // http://www.matt-helps.com/android-browser-auto-fit-fix/
 
 (function() {
-    var ua = navigator.userAgent.toLowerCase();
-    var is4xAndroid = ua.indexOf('android 4.') > -1;
-    var isNativeAndroidBrowser = ua.indexOf('mozilla/5.0') > -1 && ua.indexOf('android ') > -1 && ua.indexOf('applewebkit') > -1 && ua.indexOf('chrome') < 0;
+  var ua                     = navigator.userAgent.toLowerCase();
+  var is4xAndroid            = ua.indexOf('android 4.') > -1;
+  var isNativeAndroidBrowser = ua.indexOf('mozilla/5.0') > -1 && 
+                               ua.indexOf('android ') > -1 && 
+                               ua.indexOf('applewebkit') > -1 && 
+                               ua.indexOf('chrome') < 0;
 
-    if (is4xAndroid && isNativeAndroidBrowser) {
-        var head = document.getElementsByTagName('head')[0];
-        var style = document.createElement('style');
-        style.setAttribute('type', 'text/css');
-        style.innerHTML = 'div,p,h1,h2,h3,h4,h5,h6,section,header,footer,ul,ol,dl,pre,blockquote{' +
-            'background-image: url(data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==);' +
-            'background-repeat: repeat;}';
-        head.appendChild(style);
+  if (is4xAndroid && isNativeAndroidBrowser) {
+    var head = document.getElementsByTagName('head')[0];
+    if (head != null) {
+      var style = document.createElement('style');
+      style.setAttribute('type', 'text/css');
+      
+      // https://developer.mozilla.org/ru/docs/Web/HTML/Block-level_elements
+      var blockElements = 'address,article,aside,audio,blockquote,canvas,' +
+                          'dd,div,dl,fieldset,figcaption,figure,figure,figcaption,' +
+                          'footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,hr,li,main,' +
+                          'nav,noscript,ol,output,p,pre,section,table,tfoot,ul,video';
+      
+      var imageDataURI  = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
+      
+      style.innerHTML   = blockElements + '{' + 
+                            'background-image:  url(' + imageDataURI + ');' + 
+                            'background-repeat: repeat; ' +
+                          '}';
+      head.appendChild(style);
     }
+  }
 })();
